25d2fc89ee24cb8560d52d58a6f1d99207d72e8c
harshmistry3000 <harshmistry3000@gmail.com>
2026-03-01 18:36:22 +0530

    Configure project for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/deomo2-/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
