a3c25c6189a6b81128f23f29f3815996edde0dba
harshmistry3000 <harshmistry3000@gmail.com>
2026-03-01 18:29:27 +0530

    Configure gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/deomo2-/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
